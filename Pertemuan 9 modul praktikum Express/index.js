1f25f390ed0a5608fe8e1d62d8dced61df1301aa
StevenTjayadi <steven.535180085@stu.untar.ac.id>
2020-11-16 16:01:02 +0700

    Pertemuan 9 Express Revisi

---
+++ Pertemuan 9 modul praktikum Express/index.js
@@ -0,0 +1,21 @@
+//const express = require('express')
+
+//express().listen('3000' , () =>{
+//    console.log("Server ini berjalan"); 
+//})
+
+var express = require('express');
+
+var app = express();
+
+app.get('/' , function(req,res){res.send('Helloworld')})
+app.post('/', function(req,res){res.send('Ini Post Request')})
+app.put('/user' , function(req,res){res.send('PUT Request dijalankan')})
+app.delete('/user' , function(req,res){res.send('Delete Request pada suatu user!')})
+
+app.use(express.static('public'));
+
+// specified port
+
+app.listen(3000);
+
